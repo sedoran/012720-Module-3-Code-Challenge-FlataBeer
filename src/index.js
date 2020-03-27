8b75d23e83f9daaa531f2d8df894bd39415ec591
aprilfaithdays <aprilfaith.e@gmail.com>
2020-03-27 12:36:13 -0400

    trying once more post review

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const beerUrl = "http://localhost:3000/beers"
 document.addEventListener("DOMContentLoaded", function() {
     // console.log("Happy Friday")
     getBeers()
+    
 })
 
 const getBeers = () => {
@@ -39,17 +40,39 @@ const singleBeer = () => {
 const getBeer = (beerId) =>{
     fetch(`${beerUrl}/${beerId}`)
     .then(res => res.json())
-    .then(beer => singleBeerDetails(beer))
+    .then(beer => {
+        singleBeerDetails(beer),
+        removeBeer()
+    })
 }
 
 const singleBeerDetails = (beer) => {
     const div = document.getElementById('beer-detail')
     div.innerHTML = `
-        <h1>${beer.name}</h1>
+        <h1 id="${beer.id}">${beer.name}</h1>
         <img src="${beer.image_url}">
         <h3>${beer.tagline}</h3>
         <p>${beer.description}</p>
+        <button id=“delete-beer” class=“btn btn-info”>Delete</button>
     `
-    console.log("inside beerDetail",div)
     return div
 }
+
+const removeBeer = () =>{
+    const button = document.querySelector("button")
+    button.addEventListener("click", e => {
+        const beer = e.target.parentNode.querySelector("h1").id
+        const beerId = parseInt(beer)
+        // console.log(beerId)
+        destroyBeer(beerId)
+    })
+}
+
+
+const destroyBeer = beerId => {
+    fetch( `${beerUrl}/${beerId}`, {
+        method: "DELETE"
+    })
+    .then(console.log)
+}
+
