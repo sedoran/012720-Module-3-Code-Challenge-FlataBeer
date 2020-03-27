a7236853f71886761094d478d5b5128bd69d1915
sekoudosso82 <sekou.dosso82@gmail.com>
2020-03-27 10:40:43 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -17,8 +17,37 @@ document.addEventListener('DOMContentLoaded', () => {
     function addBeersToBrowser(beer){
         let ul = document.getElementById("beer-list")
         let li = document.createElement('li')
+        li.dataset.id = beer.id
         li.innerHTML = `<li class="list-group-item">${beer.name}</li>`
         ul.appendChild(li)
+    }
+
+    // add listener for each beer and fetch the detail about specific beer
+
+    document.addEventListener('click', function(event){
+        let specificBeerId = event.target.parentNode.dataset.id 
+        console.log(specificBeerId)
+
+        // fetch detail about specific beer 
+        fetchBeerDetail(specificBeerId)
+    })
+
+    function fetchBeerDetail(specificBeerId){
+        fetch(`http://localhost:3000/beers/${specificBeerId}`)
+        .then(res => res.json())
+        // .then(console.log)
+        .then(beerDetail =>{
+            addBeerDetailToBrowser(beerDetail)
+        })
+    }
+
+    function addBeerDetailToBrowser(beerDetail){
+        let div = document.getElementById("beer-detail")
+        div.dataset.id = beerDetail.id
+        div.innerHTML = `<h1>${beerDetail.name}</h1>
+                         <img src=${beerDetail.image_url}>
+                         <h3>${beerDetail.tagline}</h3>
+                         <p>${beerDetail.description}</p>`
     }
     
 
