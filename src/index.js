8a8c9ba8c19c33e2cf8188e12302da2725aab3b1
komald4 <komald.4@gmail.com>
2020-03-27 10:55:53 -0400

    FinalKD

--- src/index.js
+++ src/index.js
@@ -1,27 +1,16 @@
-document.addEventListener("DOMContentLoaded", function(){})
-
-console.log("Hello")
+document.addEventListener("DOMContentLoaded", () => {
+    
+    console.log("Hello")
 
 
 //Display All Beer Names
 fetch("http://localhost:3000/beers")
-.then(result => result.json())
+.then(response => response.json())
 .then(json => {
-    console.log("Image")
-    BeerList(json)
+    console.log(json)
+   const name = document.getElementById('beer-list')
+   name.innerText = json.name
+   
 })
 
-function BeerList(beers){
-    for(let i =0; i <beers.length; i++){
-        addBeerToList(beers[i])
-    }
-}
-
-function addBeerToList(book){
-    let li = document.createElement("li")
-    li.textContent = beer.title;
-    li.addEventListener("click", (e) => {
-        displayBeer(beer)
-    })
-    list.appendChil(li)
-}
+})
